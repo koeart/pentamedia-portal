42791e0f50eda67f801de7c61ab89d276b223988
dodo <dodo@blacksec.org>
2011-10-29 04:06:05 +0200

    this is more accurate

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 pentamediaportal = "http://pentamedia.org"
-cwebgitrepository = "http://www.c3d2.de/c3d2-web.git"
+cwebgitrepository = "git://git.hq.c3d2.de/c3d2-web/git.git"
 codetuberepository = "https://codetu.be/c3d2/c3d2web.git"
 cwebnewsfolder = "content/news/"
